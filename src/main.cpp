a44d4ca112e1dbe33c8aad14ed10dda4c0a881b1
H1rono <hronok66@gmail.com>
2023-03-18 22:25:39 +0900

    expand -t 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "schneider_model.h"
 
 int main() {
-	omniboat::Schneider schneider;
-	schneider.init();
-	schneider.debug();
-	while (1) {
-		schneider.led(1);
-		schneider.one_step();
-		// wait(1);
-	}
+    omniboat::Schneider schneider;
+    schneider.init();
+    schneider.debug();
+    while (1) {
+        schneider.led(1);
+        schneider.one_step();
+        // wait(1);
+    }
 }
 
