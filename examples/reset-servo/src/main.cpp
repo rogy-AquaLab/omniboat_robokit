27f8be9d7bca0762aadbd78fb02d9ebda65fc268
H1rono <hronok66@gmail.com>
2023-04-29 19:49:11 +0900

    Format

--- examples/reset-servo/src/main.cpp
+++ examples/reset-servo/src/main.cpp
@@ -1,12 +1,13 @@
 #include <chrono>
+
 #include "mbed.h"
 #include "rtos.h"
 
 int main() {
-	PwmOut servo(PB_4);
-	servo.period_ms(20);
-	servo.pulsewidth_us(1450);
-	while (1) {}
-	return 0;
+    PwmOut servo(PB_4);
+    servo.period_ms(20);
+    servo.pulsewidth_us(1450);
+    while (1) {
+    }
+    return 0;
 }
-
