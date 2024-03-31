6e5b7a5e93555fd4890b5ca8dc00ae086b8ce0e1
rein0925 <mo.kensei0925@gmail.com>
2024-03-31 23:26:06 +0900

    switch を無視するように変更

--- src/schneider_model.cpp
+++ src/schneider_model.cpp
@@ -209,11 +209,13 @@ void Schneider::rotate() {
 }
 
 void Schneider::led(int num) {
+    // NOLINTBEGIN
     switch (num) {
     case 1: led1 = !led1; break;
     case 2: led2 = !led2; break;
     case 3: led3 = !led3; break;
     default: break;
+    // NOLINTEND
     }
 }
 
