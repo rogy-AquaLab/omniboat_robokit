0febcef8645339208cd7b974798a5452a124a35a
H1rono <hronok66@gmail.com>
2024-05-09 00:16:39 +0900

    :art: Fix format

--- include/trace.hpp
+++ include/trace.hpp
@@ -2,14 +2,16 @@
 #define OMNIBOAT_ROBOKIT_TRACE_HPP
 
 namespace trace {
-enum class LedId: uint8_t {
-    First, Second, Third
+enum class LedId : uint8_t {
+    First,
+    Second,
+    Third
 };
 
 void init();
 void toggle(LedId);
 void on(LedId);
 void off(LedId);
-}
+}  // namespace trace
 
 #endif  // OMNIBOAT_ROBOKIT_TRACE_HPP
