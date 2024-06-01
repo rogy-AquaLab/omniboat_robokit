2f9102b31ddf201a1a4b293cdb8870ab8d6f430c
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-06-01 16:00:35 +0900

    ヘッダーファイル

--- include/device/input.hpp
+++ include/device/input.hpp
@@ -36,9 +36,26 @@ public:
      * @param volume_pin ツマミのピン
      * @param mpu_pins MPU6050のピン (sda, scl)
      */
+    class Builder {
+    private:
+        std::pair<PinName, PinName> joy_pins;
+        PinName volume_pin;
+        std::pair<PinName, PinName> mpu_pins;
+
+    public:
+        auto joy_pins(const PinName& pin) -> Builder&;
+        auto volume_pin(const PinName& pin) -> Builder&;
+        auto mpu_pins(const PinName& pin) -> Builder&;
+    };
+
+private:
     InputModules(
         const std::pair<PinName, PinName>& joy_pins, const PinName& volume_pin,
         const std::pair<PinName, PinName>& mpu_pins);
+
+public:
+    static auto builder() -> Builder;
+    InputModules(Builder builder);
 
     InputModules() = delete;
     ~InputModules() = default;
