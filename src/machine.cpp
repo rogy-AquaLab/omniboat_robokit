3cedd60238f63867e9e9c45b96fb9b14f00a9c12
23-saho <sahonorth@gmail.com>
2025-03-21 01:48:34 +0900

    move

--- src/machine.cpp
+++ src/machine.cpp
@@ -27,13 +27,13 @@ constexpr float step_width_a = 0.1;
 
 Machine::Machine() :
     output_modules({PB_4, PA_11}, {PA_9, PA_10}), service() {
-    device::InputModules input_modules = device::InputModules::Builder()
+    device::InputModules input_modules = std::move(device::InputModules::Builder()
         .joy_x_pin(A4)
         .joy_y_pin(A5)
         .volume_pin(A6)
         .mpu_sda_pin(D4)
         .mpu_scl_pin(D5)
-        .build();
+        .build());
     trace::toggle(LedId::First);
     trace::toggle(LedId::Second);
     trace::toggle(LedId::Third);
