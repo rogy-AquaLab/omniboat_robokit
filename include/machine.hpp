5fe93aa50b610479d9e1e98763a9dbb8048a42d9
23-saho <sahonorth@gmail.com>
2025-04-02 11:27:45 +0900

    MachineのBuilderを作成

--- include/machine.hpp
+++ include/machine.hpp
@@ -18,13 +18,14 @@ namespace omniboat {
  */
 class Machine {
 public:
-    Machine();
     Machine(const Machine&) = delete;
     Machine(Machine&&) = default;
     auto operator=(const Machine&) -> Machine& = delete;
     auto operator=(Machine&&) -> Machine& = default;
     ~Machine();
     void debug();
+
+    Machine(std::unique_ptr<device::InputModules>&& inputmodules,std::unique_ptr<device::OutputModules>&& outputmodules);
 
     /**
      * @brief 1ステップ毎にモータへの入力値を計算する関数
@@ -40,6 +41,46 @@ public:
     //  */
     // void flip_shneider();
 
+    /**
+     * @brief Machineをbuildするクラス
+     * @param _joy_x_pin ジョイスティックのピン(x)
+     * @param _joy_y_pin ジョイスティックのピン(y)
+     * @param volume_pin ツマミのピン
+     * @param _mpu_sda_pin MPU6050のピン (sda)
+     * @param _mpu_scl_pin MPU6050のピン (scl)
+     * @param servo_pins サーボのピン2つ
+     * @param dc_motor_pins DCモーター (につながっているFET) のピン2つ
+     */
+    class Builder {
+    private:
+        PinName _joy_x_pin;
+        PinName _joy_y_pin;
+        PinName _volume_pin;
+        PinName _mpu_sda_pin;
+        PinName _mpu_scl_pin;
+        std::pair<PinName,PinName> _servo_pins;
+        std::pair<PinName,PinName> _dc_motor_pins;
+
+    public:
+        auto joy_x_pin(const PinName& pin) -> Builder&;
+        auto joy_y_pin(const PinName& pin) -> Builder&;
+        auto volume_pin(const PinName& pin) -> Builder&;
+        auto mpu_sda_pin(const PinName& pin) -> Builder&;
+        auto mpu_scl_pin(const PinName& pin) -> Builder&;
+        auto servo_pins(const std::pair<PinName,PinName>& pins) -> Builder&;
+        auto dc_motor_pins(const std::pair<PinName,PinName>& pins) -> Builder&;
+        auto build() -> Machine;
+        
+        Builder() = default;
+        ~Builder() = default;
+        Builder(const Builder&) = delete;
+        auto operator=(const Builder&) -> Builder& = delete;
+        Builder(Builder&&) = default;
+        auto operator=(Builder&&) -> Builder& = default;
+};
+
+static auto builder() -> Builder;
+
 private:
     // flip_shneider と同様
     // /**
@@ -47,8 +88,8 @@ private:
     //  */
     // void ticker_flip();
 
-    device::InputModules input_modules;
-    device::OutputModules output_modules;
+    std::unique_ptr<device::InputModules> input_modules;
+    std::unique_ptr<device::OutputModules> output_modules;
     service::Service service;
 };
 
