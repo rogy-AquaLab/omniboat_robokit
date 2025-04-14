db2e957350c45d838810e2f1b137c925a90c331d
23-saho <sahonorth@gmail.com>
2025-04-14 19:08:42 +0900

    途中

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,17 @@
 
 auto main() -> int {
     trace::init();
-    omniboat::Machine machine;
+    omniboat::Machine machine(device::InputModules::builder()
+    .joy_x_pin(A4)
+    .joy_y_pin(A5)
+    .volume_pin(A6)
+    .mpu_sda_pin(D4)
+    .mpu_scl_pin(D5)
+    .build(),
+    device::OutputModules::builder()
+    .servo_pins({PB_4, PA_11})
+    .dc_motor_pins({PA_9, PA_10})
+    .build());
     machine.init();
     machine.debug();
     while (true) {
